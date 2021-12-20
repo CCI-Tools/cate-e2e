783503326e627931a1065a920b34a78cdbaf4a5a
AliceBalfanz <alicja.balfanz@brockmann-consult.de>
2021-12-20 14:47:54 +0100

    Including automatic cleanup of files older than 14 days

--- testing-cci-datasets/test_cci_data_support.py
+++ testing-cci-datasets/test_cci_data_support.py
@@ -3,16 +3,22 @@ import json
 import os
 import pandas as pd
 import random
+import shutil
 import signal
 import sys
 import traceback
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import nest_asyncio
 import numpy as np
+import pandas as pd
+from cate.core import DATA_STORE_POOL
 from cate.core import DATA_STORE_POOL
 from cate.ops.io import open_dataset
 from cate.core.ds import DataAccessError
+from cate.ops.io import open_dataset
+from xcube.core.store import DATASET_TYPE
+from xcube.core.store import DataStoreError
 from xcube.core.store import DATASET_TYPE
 from xcube.core.store import DataStoreError
 
@@ -40,13 +46,16 @@ vector_data = [
 # time out in order to cancel datasets which are taking longer than a certain time
 TIMEOUT_TIME = 120
 
+date_today = datetime.date(datetime.now())
+
 
 class TimeOutException(Exception):
     pass
 
 
 def alarm_handler(signum, frame):
-    print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] ALARM signal received')
+    print(
+        f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] ALARM signal received')
     raise TimeOutException(f'Time out after {TIMEOUT_TIME} seconds.')
 
 
@@ -73,7 +82,8 @@ def update_csv(results_csv, header_row, results_for_dataset_collection):
 def get_time_range(data_descriptor, data):
     time_range = data_descriptor.time_range
     if time_range is not None and data_descriptor.dims is not None \
-            and 'time' in data_descriptor.dims and data_descriptor.dims['time'] > 2 \
+            and 'time' in data_descriptor.dims and data_descriptor.dims[
+        'time'] > 2 \
             and data_descriptor.time_period is not None:
         # we have to use this weird notation so pandas interpretes the data correctly
         time_value = int(data_descriptor.time_period[:-1])
@@ -157,11 +167,15 @@ def check_for_processing(dataset, summary_row, time_range):
                 first = dataset.time.values[0].strftime("%Y-%m-%d %H:%M:%S")
                 last = dataset.time.values[-1].strftime("%Y-%m-%d %H:%M:%S")
             except AttributeError:
-                first = pd.to_datetime(dataset.time.values[0]).strftime("%Y-%m-%d %H:%M:%S")
-                last = pd.to_datetime(dataset.time.values[-1]).strftime("%Y-%m-%d %H:%M:%S")
+                first = pd.to_datetime(dataset.time.values[0]).strftime(
+                    "%Y-%m-%d %H:%M:%S")
+                last = pd.to_datetime(dataset.time.values[-1]).strftime(
+                    "%Y-%m-%d %H:%M:%S")
             except TypeError:
-                first = pd.to_datetime(dataset.time.values[0]).strftime("%Y-%m-%d %H:%M:%S")
-                last = pd.to_datetime(dataset.time.values[-1]).strftime("%Y-%m-%d %H:%M:%S")
+                first = pd.to_datetime(dataset.time.values[0]).strftime(
+                    "%Y-%m-%d %H:%M:%S")
+                last = pd.to_datetime(dataset.time.values[-1]).strftime(
+                    "%Y-%m-%d %H:%M:%S")
             print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] '
                   f'The requested time range is {time_range}. '
                   f'The cubes actual first time stamp is {first} '
@@ -179,7 +193,8 @@ def check_for_processing(dataset, summary_row, time_range):
     return summary_row, comment_1
 
 
-def check_write_to_disc(summary_row, comment_2, data_id, time_range, variables, region, lds,
+def check_write_to_disc(summary_row, comment_2, data_id, time_range, variables,
+                        region, lds,
                         store_name):
     signal.signal(signal.SIGALRM, alarm_handler)
     signal.alarm(TIMEOUT_TIME)
@@ -226,7 +241,8 @@ def _all_tests_no(summary_row,
     summary_row['open_bbox(3)'] = 'no' if comment_spatial else 'yes'
     summary_row['cache(4)'] = 'no'
     summary_row['map(5)'] = 'no'
-    summary_row['comment'] = general_comment if general_comment is not None else ''
+    summary_row[
+        'comment'] = general_comment if general_comment is not None else ''
     if comment_temporal is not None and comment_temporal != 'not_tested':
         if comment_spatial is not None and comment_spatial != 'not_tested':
             summary_row['comment'] = \
@@ -351,14 +367,16 @@ def test_open_ds(data_id, store, lds, results_csv, store_name):
     if data_descriptor.data_vars is not None:
         if len(data_descriptor.data_vars) > 3:
             while len(var_list) < 1:
-                for var in random.choices(list(data_descriptor.data_vars.keys()), k=2):
+                for var in random.choices(
+                        list(data_descriptor.data_vars.keys()), k=2):
                     var_list.append(var)
         else:
             var_list = list(data_descriptor.data_vars.keys())
 
     try:
-        print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] Opening cube for '
-              f'data_id {data_id} with {var_list}.')
+        print(
+            f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] Opening cube for '
+            f'data_id {data_id} with {var_list}.')
         dataset = open_dataset(ds_id=data_id,
                                data_store_id=store_name,
                                var_names=var_list,
@@ -381,13 +399,15 @@ def test_open_ds(data_id, store, lds, results_csv, store_name):
                                                      None,
                                                      var_list,
                                                      None)
-        _all_tests_no(summary_row, results_csv, general_comment=traceback_file_url)
+        _all_tests_no(summary_row, results_csv,
+                      general_comment=traceback_file_url)
         return
 
     if time_range is not None:
         try:
-            print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] Opening cube for '
-                  f'data_id {data_id} with {var_list} and time range {time_range}.')
+            print(
+                f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] Opening cube for '
+                f'data_id {data_id} with {var_list} and time range {time_range}.')
             dataset = open_dataset(ds_id=data_id,
                                    data_store_id=store_name,
                                    time_range=time_range,
@@ -402,7 +422,8 @@ def test_open_ds(data_id, store, lds, results_csv, store_name):
             else:
                 summary_row['open_temp(2)'] = 'yes'
         except:
-            comment_temporal = generate_traceback_file(store_name, data_id, time_range, var_list,
+            comment_temporal = generate_traceback_file(store_name, data_id,
+                                                       time_range, var_list,
                                                        None, '_temp')
 
     region = get_region(data_descriptor)
@@ -411,8 +432,9 @@ def test_open_ds(data_id, store, lds, results_csv, store_name):
         comment_spatial = 'Could not determine region subset.'
     else:
         try:
-            print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] Opening cube for data_id '
-                  f'{data_id} with {var_list} and region {region}.')
+            print(
+                f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] Opening cube for data_id '
+                f'{data_id} with {var_list} and region {region}.')
             dataset = open_dataset(ds_id=data_id,
                                    data_store_id=store_name,
                                    var_names=var_list,
@@ -427,15 +449,18 @@ def test_open_ds(data_id, store, lds, results_csv, store_name):
             else:
                 summary_row['open_bbox(3)'] = 'yes'
         except ValueError:
-            comment_spatial = generate_traceback_file(store_name, data_id, None, var_list, region,
-                                                  '_spatial')
+            comment_spatial = generate_traceback_file(store_name, data_id, None,
+                                                      var_list, region,
+                                                      '_spatial')
         except IndexError:
             print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] '
                   f'Index error happening at stage 2. for {data_id}')
-            comment_spatial = generate_traceback_file(store_name, data_id, None, var_list, region,
+            comment_spatial = generate_traceback_file(store_name, data_id, None,
+                                                      var_list, region,
                                                       '_spatial')
         except:
-            comment_spatial = generate_traceback_file(store_name, data_id, None, var_list, region,
+            comment_spatial = generate_traceback_file(store_name, data_id, None,
+                                                      var_list, region,
                                                       '_spatial')
 
     if comment_temporal is not None or comment_spatial is not None:
@@ -454,17 +479,21 @@ def test_open_ds(data_id, store, lds, results_csv, store_name):
 
     print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] '
           f'Checking dataset for data_id {data_id} for processing.')
-    summary_row, comment_1 = check_for_processing(dataset, summary_row, time_range)
+    summary_row, comment_1 = check_for_processing(dataset, summary_row,
+                                                  time_range)
     print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] '
           f'Checking dataset for data_id {data_id} for visualization.')
-    summary_row, comment_3 = check_for_visualization(dataset, summary_row, var_list)
+    summary_row, comment_3 = check_for_visualization(dataset, summary_row,
+                                                     var_list)
     print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] '
           f'Closing dataset for data_id {data_id}')
     dataset.close()
     print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] '
           f'Checking dataset {data_id} for writing to disk.')
-    summary_row, comment_2 = check_write_to_disc(summary_row, None, data_id, time_range,
-                                                 var_list, region, lds, store_name)
+    summary_row, comment_2 = check_write_to_disc(summary_row, None, data_id,
+                                                 time_range,
+                                                 var_list, region, lds,
+                                                 store_name)
 
     if comment_1 and (comment_1 == comment_3):
         summary_row['comment'] = f'{comment_1}'
@@ -482,17 +511,19 @@ def test_open_ds(data_id, store, lds, results_csv, store_name):
     update_csv(results_csv, header_row, summary_row)
 
 
-def generate_traceback_file(store_name, data_id, time_range, var_list, region, suffix=''):
+def generate_traceback_file(store_name, data_id, time_range, var_list, region,
+                            suffix=''):
     if not os.path.exists(f'{store_name}/error_traceback'):
         os.mkdir(f'{store_name}/error_traceback/')
-    dir_for_traceback = f'{store_name}/error_traceback/{datetime.date(datetime.now())}'
+    dir_for_traceback = f'{store_name}/error_traceback/{date_today}'
     if not os.path.exists(dir_for_traceback):
         os.mkdir(dir_for_traceback)
     traceback_file = f'{dir_for_traceback}/{data_id}{suffix}.txt'
     with open(traceback_file, 'a') as trace_f:
-        trace_f.write(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] Request: \n'
-                      f'open_dataset(ds_id={data_id}, store_id={store_name}, '
-                      f'time_range={time_range}, var_names={var_list}, region={region})\n')
+        trace_f.write(
+            f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] Request: \n'
+            f'open_dataset(ds_id={data_id}, store_id={store_name}, '
+            f'time_range={time_range}, var_names={var_list}, region={region})\n')
         trace_f.write('\n')
         trace_f.write(traceback.format_exc())
     traceback_file_url = \
@@ -687,14 +718,32 @@ def create_dict_of_ids_with_verification_flags(data_sets):
     return dict_with_verify_flags
 
 
+def cleanup_result_outputs_than_14_days(path_to_check_for_cleanup):
+    date_to_be_kept = date_today - (timedelta(days=14))
+    for item in os.listdir(path_to_check_for_cleanup):
+        try:
+            date_of_item = datetime.strptime(item[:10], '%Y-%m-%d')
+        except ValueError:
+            continue
+        if date_of_item and date_of_item.date() < date_to_be_kept:
+            try:
+                if os.path.isdir(os.path.join(path_to_check_for_cleanup, item)):
+                    shutil.rmtree(os.path.join(path_to_check_for_cleanup, item))
+                if os.path.isfile(
+                        os.path.join(path_to_check_for_cleanup, item)):
+                    os.remove(os.path.join(path_to_check_for_cleanup, item))
+            except OSError as e:
+                print(f'Error removing {item}: {e.strerror}')
+
+
 def main():
     store_name = 'cci-store'
     if len(sys.argv) == 2:
         store_name = sys.argv[1]
     if not os.path.exists(store_name):
         os.mkdir(store_name)
-    support_file_name = f'test_{store_name}_data_support_{datetime.date(datetime.now())}.csv'
-    results_csv = f'{store_name}/{support_file_name}'
+    support_file_name = f'{date_today}_test_{store_name}_data_support'
+    results_csv = f'{store_name}/{support_file_name}.csv'
     store = DATA_STORE_POOL.get_store(store_name)
     data_ids = store.get_data_ids()
     lds = DATA_STORE_POOL.get_store('local')
@@ -703,40 +752,50 @@ def main():
     for data_id in data_ids:
         test_open_ds(data_id, store, lds, results_csv, store_name)
 
-    sort_csv(results_csv, f'{store_name}/sorted_{support_file_name}')
+    sort_csv(results_csv, f'{store_name}/{support_file_name}_sorted.csv')
 
-    test_data_sets = read_all_result_rows(f'{store_name}/sorted_{support_file_name}', header_row)
+    test_data_sets = read_all_result_rows(
+        f'{store_name}/{support_file_name}_sorted.csv', header_row)
 
     ecvs = get_list_of_ecvs(test_data_sets)
-    failed_csv = f'{store_name}/failed_{support_file_name}'
+    failed_csv = f'{store_name}/{support_file_name}_failed.csv'
     create_list_of_failed(test_data_sets, failed_csv, header_row)
     if os.path.exists(failed_csv):
-        sort_csv(failed_csv, f'{store_name}/sorted_failed_{support_file_name}')
+        sort_csv(failed_csv,
+                 f'{store_name}/{support_file_name}_sorted_failed.csv')
 
     with open(results_csv, 'r', newline='') as f_input:
         csv_input = csv.DictReader(f_input)
         data = sorted(csv_input, key=lambda row: (row['Dataset-ID']))
-    with open(f'{store_name}/sorted_{support_file_name}', 'w', newline='') as f_output:
+    with open(f'{store_name}/{support_file_name}_sorted.csv', 'w',
+              newline='') as f_output:
         csv_output = csv.DictWriter(f_output, fieldnames=csv_input.fieldnames)
         csv_output.writeheader()
         csv_output.writerows(data)
 
-    summary_csv = f'{store_name}/summary_sorted_{support_file_name}'
-    header_summary = ['ecv', 'supported', 'open_success', 'open_fail', 'open_temp_success',
-                      'open_temp_fail', 'open_bbox_success', 'open_bbox_fail', 'cache_success',
-                      'cache_fail', 'visualize_success', 'visualize_fail', 'supported_percentage',
+    summary_csv = f'{store_name}/{support_file_name}_summary_sorted.csv'
+    header_summary = ['ecv', 'supported', 'open_success', 'open_fail',
+                      'open_temp_success',
+                      'open_temp_fail', 'open_bbox_success', 'open_bbox_fail',
+                      'cache_success',
+                      'cache_fail', 'visualize_success', 'visualize_fail',
+                      'supported_percentage',
                       'open_success_percentage', 'open_temp_success_percentage',
-                      'open_bbox_success_percentage', 'cache_success_percentage',
-                      'visualize_success_percentage', 'total_number_of_datasets']
+                      'open_bbox_success_percentage',
+                      'cache_success_percentage',
+                      'visualize_success_percentage',
+                      'total_number_of_datasets']
 
     for ecv in ecvs:
         results_summary_row = count_success_fail(test_data_sets, ecv)
         update_csv(summary_csv, header_summary, results_summary_row)
 
-    dict_with_verify_flags = create_dict_of_ids_with_verification_flags(test_data_sets)
+    dict_with_verify_flags = create_dict_of_ids_with_verification_flags(
+        test_data_sets)
 
     with open(f'{store_name}/'
-              f'DrsID_verification_flags_{datetime.date(datetime.now())}.json', 'w') as f:
+              f'{date_today}_DrsID_verification_flags.json',
+              'w') as f:
         json.dump(dict_with_verify_flags, f, indent=4)
 
     if os.path.exists(results_csv):
@@ -751,11 +810,14 @@ def main():
         print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] '
               f'The file {failed_csv} does not exist.')
 
+    cleanup_result_outputs_than_14_days(store_name)
+    cleanup_result_outputs_than_14_days(f'{store_name}/error_traceback')
+
     print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] '
-          f'Test run finished on {datetime.date(datetime.now())}.')
+          f'Test run finished on {date_today}.')
     print(f'[{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}] '
           f'Test run took {datetime.now() - start_time}')
 
 
 if __name__ == '__main__':
-     main()
+    main()
